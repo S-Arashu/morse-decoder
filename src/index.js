e0277e2095d42bdc59909b505ba50588ee43aadd
S-Arashu <118382286+S-Arashu@users.noreply.github.com>
2025-09-05 00:14:29 +0300

    feat: solve morse decoder task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,40 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
+module.exports = function decode(expr) {
+  let encodedString = '';
+  let resultStr = '';
+
+  const iterations = expr.length / 10;
+  let SIMB = 10;
+  let START = 0;
+
+  for (let i = 0; i < iterations; i += 1) {
+    if (expr[START] === '*') {
+      resultStr += ' ';
+      SIMB += 10;
+      START += 10;
+      encodedString = '';
+    }
+
+    const zeroesEnd = expr.indexOf('1', START);
+
+    if (zeroesEnd !== -1) {
+      for (let j = zeroesEnd; j < SIMB; j += 2) {
+        if (expr[j] === '1' && expr[j + 1] === '0') {
+          encodedString += '.';
+        } else if (expr[j] === '1' && expr[j + 1] === '1') {
+          encodedString += '-';
+        }
+      }
+
+      resultStr += MORSE_TABLE[encodedString];
+      SIMB += 10;
+      START += 10;
+      encodedString = '';
+    }
+  }
+
+  return resultStr;
 };
